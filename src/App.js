05bfbb6eb5bc4ab95f510239c1be41336de69417
unknown <matthewpiedra1228@gmail.com>
2020-06-22 15:36:46 -0400

    Added increment, decrement methods & attached the onClick event listener

--- src/App.js
+++ src/App.js
@@ -8,14 +8,27 @@ class Counter extends Component {
     } 
   }
 
+  // this is where our methods will live: below the constructor ALWAYS !
+  // Note* arrow functions does all the lexical binding for us. If it wasn't an arrow function, 
+  // then in the constructor we would have to create another property that
+  // binds the method to the this context: 
+  // ex this.increment = this.increment.bind(this);
+  increment = () => {
+     // setState comes with react; it's built-in already
+     this.setState({count: this.state.count + 1});
+  }
+  decrement = () => {
+     this.setState({count: this.state.count - 1});
+  }
+
   render() {
     return (
       <div className="container"> 
         <div className="navbar"> Counter.js </div>
         <div className="Content">
            <h1> {this.state.count} </h1>
-           <button type="button"> Increment </button>
-           <button type="button"> Decrement </button>
+           <button type="button" onClick={this.increment}> Increment </button>
+           <button type="button" onClick={this.decrement}> Decrement </button>
         </div>
       </div>
     );
